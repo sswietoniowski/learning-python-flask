160bc4a336151723da92f5a24f3b457c0eb878f7
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-07-29 12:52:03 +0200

    First attempt to use Model-Template-View in Flask

--- flashcards.py
+++ flashcards.py
@@ -1,25 +1,8 @@
-from datetime import datetime
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
-    return "Welcome to my Flash Cards application!"
-
-
-@app.route("/date")
-def date():
-    return f"This page was served at {datetime.now()}"
-
-
-counter = 0
-
-
-@app.route("/count_views")
-def count_views():
-    global counter
-    counter += 1
-    return f"This page was server {counter} times"
+    return render_template("welcome.html")
