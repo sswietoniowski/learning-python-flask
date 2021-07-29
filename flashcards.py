200689ba775e1df638a5ec8e78bde8db29720f0a
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-07-29 11:45:11 +0200

    Added new endpoint to test flow of control

--- flashcards.py
+++ flashcards.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def welcome():
     return "Welcome to my Flash Cards application!"
+
+
+@app.route("/date")
+def date():
+    return f"This page was served at {datetime.now()}"
