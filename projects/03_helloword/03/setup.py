67b3904a806c17a1e7087f870994f671530bc688
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-01-23 22:28:46 +0100

    Added Bootstrap starter

--- projects/03_helloword/03/setup.py
+++ projects/03_helloword/03/setup.py
@@ -1,12 +1,19 @@
 # 3rd attempt
 #
-# TODO: describe what we are doing here
+# This version was initialized with: https://startbootstrap.com/theme/freelancer
 
+from flask import render_template
 from app import create_app
+
 
 app = create_app()
 
 
 @app.get("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")
+
+
+@app.get("/content")
+def content():
+    return render_template("content.html")
