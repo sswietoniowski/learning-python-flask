6a582717cc245078576e1626ad16922908cb3850
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-01-23 19:01:04 +0100

    Updated docs

--- projects/03_helloword/02/setup.py
+++ projects/03_helloword/02/setup.py
@@ -1,6 +1,8 @@
 # 2nd attempt at a flask app
 #
-# we've moved the app creation to a separate package
+# We've moved the app creation to a separate package
+#
+# More info about this idea here: https://flask.palletsprojects.com/en/2.2.x/patterns/appfactories/
 
 from app import create_app
 
