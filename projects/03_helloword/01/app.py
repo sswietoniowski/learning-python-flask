9d178f77c17d37585cc5a6b58ca41a618e18d662
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-01-23 14:18:03 +0100

    Code refactored

--- projects/03_helloword/01/app.py
+++ projects/03_helloword/01/app.py
@@ -1,3 +1,6 @@
+# At this stage everything (app) is in one file, after some time (with more routes) this will be a problem,
+# as this file becomes too big and hard to maintain. So we want to split it into multiple files.
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +9,11 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     return "Hello World!"
+
+
+@app.get("/something")
+def something():
+    return "Something"
 
 
 if __name__ == "__main__":
